dfa191dd014f690e21905fb456b0071de16ebeb5
Edwig <Edwig@CERT5>
2016-07-07 08:58:33 +0200

    IIS register/unregister commands

---
+++ register.cmd
@@ -0,0 +1,4 @@
+@echo off
+@echo Installing Marlin to IIS
+@C:\Windows\System32\inetsrv\appcmd.exe install module /name:MarlinIISModule /image:c:\develop\marliniis\binDebug_x64\MarlinIISModule.dll
+rem  @C:\Windows\System32\inetsrv\appcmd.exe add     module /name:MarlinIISModule /app.name:"MarlinTest"
---
+++ unregister.cmd
@@ -0,0 +1,3 @@
+@echo off
+@echo UN-Installing Marlin from IIS
+@C:\Windows\System32\inetsrv\appcmd uninstall module MarlinIISModule
