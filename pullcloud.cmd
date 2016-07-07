fbe9c1651f8358e7e03abc058183813ade17dc23
Edwig <Edwig@CERT5>
2016-07-07 09:00:15 +0200

    pull/push to cloud for version 4

--- pullcloud.cmd
+++ pullcloud.cmd
@@ -1,16 +1,21 @@
 @echo off
-@echo Cleaning the Marlin directories
+@echo Cleaning the Marlin IIS directories
 @echo .
 pause
 
 del /q /s /f *.sdf
-rmdir /q /s MarlinClient
-rmdir /q /s MarlinPServer
+del /q /s /f *.VC.db
+rmdir /q /s Certificates
+rmdir /q /s Documentation
+rmdir /q /s ExtraParts
 rmdir /q /s HTTPManager
 rmdir /q /s Marlin
-rmdir /q /s ExtraParts
-rmdir /q /s Documentation
-rmdir /q /s Certificates
+rmdir /q /s MarlinClient
+rmdir /q /s MarlinIISClient
+rmdir /q /s MarlinIISModule
+rmdir /q /s MarlinServer
+rmdir /q /s ServerTestset
+rmdir /q /s TestsetClient
 echo .
 echo Ready cleaning
 echo .
--- pushcloud.cmd
+++ pushcloud.cmd
@@ -1,5 +1,5 @@
 @echo off
-@echo Cleaning Marlin directories
+@echo Cleaning Marlin IIS directories
 @echo .
 
 del marlin_3.rar
@@ -7,15 +7,24 @@ del /q /s /f *.sdf
 del /q /s /f *.VC.db
 rmdir /q /s Marlin\Debug
 rmdir /q /s Marlin\Release
+rmdir /q /s Marlin\Win32
 rmdir /q /s Marlin\x64
 rmdir /q /s MarlinClient\ipch
 rmdir /q /s MarlinClient\Debug
 rmdir /q /s MarlinClient\Release
 rmdir /q /s MarlinClient\x64
+rmdir /q /s MarlinIISClient\ipch
+rmdir /q /s MarlinIISClient\Debug
+rmdir /q /s MarlinIISClient\Release
+rmdir /q /s MarlinIISClient\x64
 rmdir /q /s MarlinServer\ipch
 rmdir /q /s MarlinServer\Debug
 rmdir /q /s MarlinServer\Release
 rmdir /q /s MarlinServer\x64
+rmdir /q /s MarlinIISModule\ipch
+rmdir /q /s MarlinIISModule\Debug
+rmdir /q /s MarlinIISModule\Release
+rmdir /q /s MarlinIISModule\x64
 rmdir /q /s HTTPManager\Debug
 rmdir /q /s HTTPManager\Release
 rmdir /q /s HTTPManager\ipch
@@ -30,11 +39,11 @@ echo Ready cleaning
 echo .
 echo Creating a rar archive for the Marlin server
 echo .
-"C:\Program Files\Winrar\rar.exe" a Marlin_3.rar .git MarlinClient MarlinServer Marlin HTTPManager Certificates Documentation ExtraParts *.cmd
+"C:\Program Files\Winrar\rar.exe" a Marlin_4.rar .git Certificates ExtraParts Marlin MarlinClient MarlinIISClient MarlinIISModule MarlinServer ServerTestset TestsetClient HTTPManager Documentation *.cmd
 echo .
 echo Ready making RAR archive
 echo .
-robocopy . C:\Users\%USERNAME%\OneDrive\Documents Marlin_3.rar /xo
+robocopy . C:\Users\%USERNAME%\OneDrive\Documents Marlin_4.rar /xo
 echo .
 echo Ready copying the archive to the OneDrive cloud.
 pause
