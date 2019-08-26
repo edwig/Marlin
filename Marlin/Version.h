202e128b42a82ccfcecf8cb7ce3ab34bbd88ad70
Edwig Huisman <edwig.huisman@hetnet.nl>
2019-08-26 12:44:10 +0200

    Version 6.2.0 From Static to Dynamic memory model

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -29,9 +29,9 @@
 
 // Version number components
 #define MARLIN_PRODUCT_NAME     "MarlinServer"   // Our name
-#define MARLIN_VERSION_NUMBER   "6.1.2"          // The real version
+#define MARLIN_VERSION_NUMBER   "6.2.9"          // The real version
 #define MARLIN_VERSION_BUILD    ""               // Can carry strings like 'Alpha', 'Beta', 'RC'
-#define MARLIN_VERSION_DATE     "13-08-2019"     // Last production date
+#define MARLIN_VERSION_DATE     "26-08-2019"     // Last production date
 
 // This is our version string
 #define MARLIN_SERVER_VERSION MARLIN_PRODUCT_NAME " " MARLIN_VERSION_NUMBER MARLIN_VERSION_BUILD
--- MarlinModule/Installer_64Bits.nsi
+++ MarlinModule/Installer_64Bits.nsi
@@ -9,7 +9,7 @@
 ; Version:             6.1
 ;-------------------------------------------------------
  !define PRODUCT_NAME                         "Marlin IIS Module 64Bits"
- !define PRODUCT_VERSION                      "6.1.2.0"
+ !define PRODUCT_VERSION                      "6.2.0"
  !define PRODUCT_BUILDNUMBER                  "11"
  !define PRODUCT_PUBLISHER                    "Edwig Huisman"
  !define PRODUCT_WEB_SITE                     "https://github.com/Edwig/Marlin"
